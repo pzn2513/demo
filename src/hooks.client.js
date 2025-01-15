691973699b499b32e398ae7449123873918fbdc4
pzn <pznforwork@outlook.com>
2025-01-15 20:02:01 +0800

    1

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -2,3 +2,11 @@
 // 这个文件只在客户端执行
 console.log(globalThis);
 globalThis.x = {};
+
+export function handleError(error) {
+    console.error('客户端错误:', error);
+}
+
+export function init() {
+    console.log('客户端初始化');
+}
